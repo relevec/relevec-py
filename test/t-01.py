14d86e4d5f889459ac56514287447928722e2a36
Joe Sandmeyer <github.com@unconscious.com>
2024-07-17 09:22:24 -0400

    first test

---
+++ test/t-01.py
@@ -0,0 +1,21 @@
+from ../relevec import ReleVec
+
+# Create test classes and instances
+#Cn = ReleVec.get_specified_subclass('Cn', dim_names=['a','b','c'])
+
+ReleVec.import_subclasses_dict({
+    'Ci': {'dim_ct': 3},
+    'Cn': {'dim_names': ['a', 'b', 'c']}
+    })
+Cn = ReleVec.get_subclass_by_name('Cn')
+on = Cn({'a':0.01, 'b':0.11, 'c':0.21})
+
+print('Export vec instance:')
+
+print(on.export_dict())
+
+print('')
+
+print("Export subs: ", ReleVec.export_subclasses_dict())
+
+print("t-01.py test complete")
